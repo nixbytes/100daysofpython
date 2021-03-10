65965bd4fc0398ce93de39a07ae835b619422393
nixbuytes <real8686@gmail.com>
2021-03-09 23:07:06 -0500

    finish day3

--- Day3/fizzbuzz.py
+++ Day3/fizzbuzz.py
@@ -1,14 +1,9 @@
 def fizzbuzz(num):
-      result = []
-      for num in range(1,n+1):
-         if num% 3== 0 and i%5==0:
-            result.append("FizzBuzz")
-         elif num %3==0:
-            result.append("Fizz")
-         elif num% 5 == 0:
-            result.append("Buzz")
-         else:
-            result.append(str(num))
-      return resultA
-
-fizzbuzz()
+    if num % 3 == 0 and num % 5 == 0:
+        return "Fizz Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
