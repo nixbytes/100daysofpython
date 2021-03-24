1fe6327e10d8feb7c403378ca61c48f098e1fcc9
nixbuytes <real8686@gmail.com>
2021-03-23 22:29:36 -0400

    work with statistics mod

--- Day8/running_mean.py
+++ Day8/running_mean.py
@@ -1,5 +1,11 @@
+import statistics
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
-       returns a sequence of same length with the averages.
-       You can assume all items in sequence are numeric."""
-    pass
+    returns a sequence of same length with the averages.
+    You can assume all items in sequence are numeric."""
+    return [
+        round(float(statistics.mean(sequence[: x + 1])), 2)
+        for x in range(len(sequence))
+    ]
